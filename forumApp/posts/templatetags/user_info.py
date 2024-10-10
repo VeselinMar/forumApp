c7fbe85825e0584d83a0a62a669f6f6ef9e0ce98
Veselin Martionv <veselin.martinov@gmail.com>
2024-10-10 21:15:00 +0200

    include bootstrap in project

--- forumApp/posts/templatetags/user_info.py
+++ forumApp/posts/templatetags/user_info.py
@@ -3,11 +3,11 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('common/user_info.html', takes_context=True)
-def user_info(context):
-    if context.request.user.is_authenticated:
+@register.inclusion_tag('common/user_info.html')
+def user_info(user):
+    if user.is_authenticated:
         return {
-            'username': context.request.user.username,
+            'username': user.username,
         }
 
     return {
